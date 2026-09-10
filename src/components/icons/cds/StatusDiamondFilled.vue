81b776be95913e6305f554aed4196299897e1670
Niklavs-M <niklavs.mezeckis@gmail.com>
2026-09-10 15:13:29 +0300

    New icons added (!3125)

---
+++ src/components/icons/cds/StatusDiamondFilled.vue
@@ -0,0 +1,3 @@
+<template>
+  <g><path d="M16,31L1,16,16,1l15,15-15,15Z" /></g>
+</template>
---
+++ src/components/icons/material/StatusDiamondFilled.vue
@@ -0,0 +1,3 @@
+<template>
+  <g><path transform="translate(0,96)" d="M480,930L30,480,480,30l450,450-450,450Z" /></g>
+</template>
